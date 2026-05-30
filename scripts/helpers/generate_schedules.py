40b4adbc874d4b8aaeb6d7f78be6379e2246a771
Icefoxzhx <hongxinzhang59@gmail.com>
2026-05-30 00:01:32 +0000

    update helpers

--- scripts/helpers/generate_schedules.py
+++ scripts/helpers/generate_schedules.py
@@ -2,11 +2,8 @@ import argparse
 import json
 from datetime import datetime
 import os
-import sys
 import shutil
 
-current_directory = os.getcwd()
-sys.path.insert(0, current_directory)
 
 from agents.ella import EllaAgent
 from tools.model_manager import global_model_manager
@@ -26,6 +23,7 @@ fh.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
+<<<<<<< HEAD
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--scene", "-s", type=str, default='NY')
@@ -38,6 +36,20 @@ if __name__ == '__main__':
 	parser.add_argument("--curr_time", type=str)
 	parser.add_argument("--lm_source", type=str, default='openai')
 	args = parser.parse_args()
+=======
+parser = argparse.ArgumentParser()
+parser.add_argument("--scene", "-s", type=str, default='NY')
+parser.add_argument("--num_agents", "-n", type=int, default=15)
+parser.add_argument("--config", type=str, default='agents_num_15')
+parser.add_argument("--event", type=str)
+parser.add_argument("--output_dir", type=str)
+parser.add_argument("--debug", action='store_true')
+parser.add_argument("--overwrite", action='store_true')
+parser.add_argument("--curr_time", type=str)
+parser.add_argument("--lm_source", type=str, choices=["openai", "azure", "huggingface", "local"], default='openai')
+parser.add_argument("--lm_id", type=str, default='gpt-4o')
+args = parser.parse_args()
+>>>>>>> 87a9e99 (update helpers)
 
 	if not args.output_dir:
 		args.output_dir = os.path.join(ASSETS_PATH, "scenes", args.scene, args.config + "_with_schedules")
@@ -58,6 +70,7 @@ if __name__ == '__main__':
 
 	config = json.load(open(config_path, "r"))
 
+<<<<<<< HEAD
 	global_model_manager.init(local=True)
 	for i in range(args.num_agents):
 		agent = EllaAgent(name=config["agent_names"][i], pose=config["agent_poses"][i], info=config["agent_infos"][i],
@@ -76,3 +89,29 @@ if __name__ == '__main__':
 		agent.save_scratch()
 	print(f"Generated schedules for {args.num_agents} agents in {args.scene}.")
 	os._exit(0)
+=======
+config_path = os.path.join(args.output_dir, "config.json")
+if not os.path.exists(args.output_dir):
+	shutil.copytree(seed_sim_path, args.output_dir)
+
+config = json.load(open(config_path, "r"))
+
+global_model_manager.init(local=(args.lm_source == 'local'))
+for i in range(args.num_agents):
+	agent = EllaAgent(name=config["agent_names"][i], pose=config["agent_poses"][i], info=config["agent_infos"][i],
+					  sim_path=args.output_dir, no_react=True, debug=True, logger=logger, lm_source=args.lm_source, lm_id=args.lm_id, detect_interval=-1)
+	hourly_schedule_path = os.path.join(agent.storage_path, "hourly_schedule.json")
+	if os.path.exists(hourly_schedule_path):
+		print(f"Skip generating schedule for {agent.name}")
+		schedule = json.load(open(hourly_schedule_path, "r"))
+	else:
+		if args.curr_time is not None:
+			agent.set_curr_time(datetime.strptime(args.curr_time, "%H:%M:%S"))
+		schedule = agent.generate_hourly_schedule()
+		with open(hourly_schedule_path, "w") as f:
+			json.dump(schedule, f, indent=4)
+	agent.hourly_schedule = schedule
+	agent.save_scratch()
+print(f"Generated schedules for {args.num_agents} agents in {args.scene}.")
+os._exit(0)
+>>>>>>> 87a9e99 (update helpers)
